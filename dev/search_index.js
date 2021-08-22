5936d1f020fd8ab5239fbb00a94e7d4d9525ab33
Documenter.jl <documenter@juliadocs.github.io>
2021-08-22 17:30:37 +0000

    build based on 1f3fa53

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Bites","category":"page"},{"location":"#Bites","page":"Home","title":"Bites","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Bites.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Bites]","category":"page"}]
+}
